ee6470978521dd5c11ecf9964c7624096b8c0f63
Mohammed Talaat <mohammed.talaat@kickstartinteractive.net>
2020-03-02 17:00:58 +0200

    fixes

--- tests/Unit/TeamTest.php
+++ tests/Unit/TeamTest.php
@@ -65,28 +65,33 @@ class TeamTest extends TestCase
     	 $team = factory(Team::class)->create();
     	 $user = factory(User::class)->create();
     	 $team->addMember($user);
+    	 $teamCountBeforeRemove = $team->countMembers();
     	 $team->removeMember($user);
+    	 $teamCountAfterRemove = $team->countMembers();
 
     	 $userStillInTeam = $user->team_id == $team->id ? true : false;
     	 $this->assertEquals(false,$userStillInTeam);
+    	 $this->assertEquals($teamCountBeforeRemove-1,$teamCountAfterRemove);
 
     }
 	//TODO 
     /** @test */
     public function a_team_can_remove_all_memebers_at_once()
     {
-    	 $team = factory(Team::class)->create();
+    	 $team =  factory(Team::class)->create();
     	 $users = factory(User::class,2)->create();
     	 $team->addMember($users);
     	 $team->removeMember($users);
 
     	 //check if one of them still in the team
     	 $allRemoved = true;
+    	 $membersCount = $team->countMembers();
     	 foreach ($users as $member) {
     	 	if($team->id == $member->team_id)
     	 		$allRemoved = false;
     	 }
     	 $this->assertEquals(true, $allRemoved);
+    	 $this->assertEquals(0, $membersCount);
 
     }
 }
